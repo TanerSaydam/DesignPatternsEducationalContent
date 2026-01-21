f083b20fe5599d756aca9872e90d1f90baabb8d3
Taner Saydam <taner_and_you@hotmail.com>
2026-01-21 17:53:12 +0300

    Circuit Breaket Pattern

--- 20CircuitBreakerPattern/Program.cs
+++ 20CircuitBreakerPattern/Program.cs
@@ -1,9 +1,23 @@
+using FluentEmail.Core;
+using FluentEmail.Core.Models;
 using Polly;
+using Polly.Registry;
 using Polly.Retry;
+using System.Net.Mail;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpClient();
+
+builder.Services.AddFluentEmail("info@tanersaydam.com").AddSmtpSender(new SmtpClient() { Host = "localhost", Port = 25 });
+
+builder.Services.AddResiliencePipeline("http", cfr =>
+{
+    cfr.AddRetry(new RetryStrategyOptions()
+    {
+        ShouldHandle = new PredicateBuilder().Handle<SmtpException>()
+    }).Build();
+});
 
 var app = builder.Build();
 
@@ -17,6 +31,15 @@ app.MapGet("/get-todos", async (HttpClient httpClient) =>
     var todos = await res.Content.ReadFromJsonAsync<List<Todo>>();
 
     return todos;
+});
+
+app.MapGet("/send-email", async (IFluentEmail fluentEmail, ResiliencePipelineProvider<string> pipelineProvider) =>
+{
+    var res = await PipelineService.EmailPipeline.ExecuteAsync(async ct => await fluentEmail.To("my@business.com").Subject("Hello world").SendAsync());
+    //var pipeline = pipelineProvider.GetPipeline("http");
+    //var res = await pipeline.ExecuteAsync(async ct => await fluentEmail.To("my@business.com").Subject("Hello world").SendAsync());
+
+    return res.Successful;
 });
 
 app.Run();
@@ -35,9 +58,21 @@ public class PipelineService
         MaxRetryAttempts = 3, //deneme süresi
         Delay = TimeSpan.FromSeconds(5), //her denemede bu kadar bekle
         ShouldHandle = new PredicateBuilder<HttpResponseMessage>() //sonucu nasıl işleyeceği
-            .Handle<HttpRequestException>()
-            .HandleResult(r => !r.IsSuccessStatusCode) // 404/500 vs retry
+            .Handle<HttpRequestException>() //exception fırltatırsa tekrar dene
+            .HandleResult(r => !r.IsSuccessStatusCode) //exception fırlatmasa bile dönen sonuç buysa başarısız say
     })
     .AddTimeout(TimeSpan.FromSeconds(20)) //Timeout, bir deneme 20 saniyeyi aşarsa iptal eder.
     .Build();
+
+    public static ResiliencePipeline<SendResponse> EmailPipeline = new ResiliencePipelineBuilder<SendResponse>()
+        .AddRetry(new RetryStrategyOptions<SendResponse>
+        {
+            MaxRetryAttempts = 3,
+            Delay = TimeSpan.FromSeconds(5),
+            ShouldHandle = new PredicateBuilder<SendResponse>()
+            .Handle<SmtpException>() //exception fırltatırsa tekrar dene
+            //.HandleResult(r => !r.Successful) //exception fırlatmasa bile dönen sonuç buysa başarısız say
+        })
+        .AddTimeout(TimeSpan.FromSeconds(20))
+        .Build();
 }
